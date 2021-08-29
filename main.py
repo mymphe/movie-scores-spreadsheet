a582a9729d447024056f4802f1b24f9175ea90ad
mymphe <azatsdev@gmail.com>
2021-08-29 15:10:34 +0600

    add comments for google auth

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import gspread
 def main():
     print('main')
 
+    # connect google service account, sourced from ~/.config/gspread/service_account.json
     gc = gspread.service_account()
+
+    # open spreadsheet by name
     sh = gc.open("movie_ratings")
 
     print(sh.sheet1.get('A1'))
